f7aee21dfb3df39735dd8efcfdada861ba5ebd69
Justin Dang <justin.ht.dang@gmail.com>
2017-12-27 18:55:50 +1100

    finally flippable and tiltable card!

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 
 $(document).ready(function(event) {
-    
+
     $(".scroll-link").on('click', function() {
-        
+
         var linkTo = $(this).attr('name');
 
         $('html, body').animate( {
             scrollTop: $(linkTo).offset().top
-        }, 400);
+        }, 300);
+    });
+
+    $('.flipper').flip({
+        trigger: 'click'
+    });
+
+    $('.flipper').on('mouseover', function () {
+        $(this).css('transform', 'rotateY(30deg)')
+    });
+
+    $('.flipper').on('mouseleave', function () {
+        $(this).css('transform', 'rotateY(0deg)')
     });
 });
